c6077dc351ef7b51efb80de537a9bef4a29b8b1c
Bryan Boreham <bryan@weave.works>
2016-07-26 16:33:09 +0100

    Warn when no CNI networks are found
    Otherwise the error gets lost

--- pkg/kubelet/network/cni/cni.go
+++ pkg/kubelet/network/cni/cni.go
@@ -56,6 +56,7 @@ func probeNetworkPluginsWithVendorCNIDirPrefix(pluginDir, vendorCNIDirPrefix str
 	configList := make([]network.NetworkPlugin, 0)
 	network, err := getDefaultCNINetwork(pluginDir, vendorCNIDirPrefix)
 	if err != nil {
+		glog.Warningf("Error finding default CNI network: %v", err)
 		return configList
 	}
 	return append(configList, &cniNetworkPlugin{
